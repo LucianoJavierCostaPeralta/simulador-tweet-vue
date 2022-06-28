364d37b9a5b8dc5a2b4bee0ed9ba803d1c805da7
luciano <luchinjeje@gmail.com>
2022-06-28 19:14:20 -0300

    Added Menu.vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <span class="navbar-brand">
+            {{ titleMenu }}
+            </span>
+
+            <button class="btn btn-success px-4">
+                {{ titleButton }}
+            </button>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            titleMenu  : 'Simulador de tweets',
+            titleButton: 'Nuevo Tweet'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
